42bb3382a4a70589fc86a1bab5c457b78cdb46ad
hacketo7s <resses153@gmail.com>
2025-06-23 14:01:29 +0530

    Updated main.lua to have formatting

--- main.lua
+++ main.lua
@@ -1,47 +1,46 @@
 --Turn on HTTP Requests in security settings of your game for this code to work.
 
-local httpService = game:GetService("HttpService") 
+local httpService = game:GetService("HttpService")
 
 local ApiKey = "" --Put your API Key here.
-local endpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="..ApiKey --The endpoint may be outdated, be cautious.
+local endpoint =
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" .. ApiKey --The endpoint may be outdated, be cautious.
 
 if ApiKey == "" then
-  print("Please get an API Key from Google Studio.")
-  return
+    print("Please get an API Key from Google Studio.")
+    return
 end
 
 local function callGemini(msg)
-  	--request
-		local requestBody = {
-			contents = {
-				parts = {
-					text = msg
-				}
-			}
-		}
+    --request
+    local requestBody = {
+        contents = {
+            parts = {
+                text = msg
+            }
+        }
+    }
 
-		local jsonEncode = httpService:JSONEncode(requestBody)
+    local jsonEncode = httpService:JSONEncode(requestBody)
 
-		--error detection and request to google servers
-		local success, response = pcall(function()
-			return 	httpService:PostAsync(
-				endpoint,
-				jsonEncode,
-				Enum.HttpContentType.ApplicationJson,
-				false
-			)
-		end)
+    --error detection and request to google servers
+    local success, response =
+        pcall(
+        function()
+            return httpService:PostAsync(endpoint, jsonEncode, Enum.HttpContentType.ApplicationJson, false)
+        end
+    )
 
     --if request was sent
-		if success then
-			local data = httpService:JSONDecode(response)
+    if success then
+        local data = httpService:JSONDecode(response)
 
-			local answer = data.candidates and data.candidates[1] and data.candidates[1].content.parts[1].text or "No reply"
-      return answer
-		else
-			warn(response)
-      return response
-		end
+        local answer = data.candidates and data.candidates[1] and data.candidates[1].content.parts[1].text or "No reply"
+        return answer
+    else
+        warn(response)
+        return response
+    end
 end
 
 --Example Usage:
